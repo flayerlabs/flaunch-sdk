df5d3f3dbfe3e81f45b42c3c092aed6ee8806dd6
apoorvlathey <latheyapoorv@gmail.com>
2025-03-03 18:48:39 +0400

    remove unused dec2hex

--- src/helpers/hex.ts
+++ src/helpers/hex.ts
@@ -1,8 +1,5 @@
 import { encodeAbiParameters, Hex, hexToBigInt, pad } from "viem";
 
-// i.e. 0-255 -> '00'-'ff'
-const dec2hex = (dec: number): string => dec.toString(16).padStart(2, "0");
-
 export const bytes32ToUint256 = (value: Hex) => {
   return hexToBigInt(value);
 };
